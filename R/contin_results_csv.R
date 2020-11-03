81dc140e03d1feb02413df810e544ecfdfb9ea51
rmichie <michie.ryan@deq.state.or.us>
2020-11-03 09:22:20 -0800

    added import as csv

--- R/contin_results_csv.R
+++ R/contin_results_csv.R
@@ -5,19 +5,33 @@
 #' The column names must be the same in every csv file.
 #'
 #' Column names are made into syntactically valid names acceptable by R.
+#' @param path The path to the directory where the csv files are saved. Default is NULL which implements the [choose.dir] function on windows OS
+#' or [rstudioapi::selectDirectory] on Mac/Linux OS when using Rstudio. If Rstudio is not avaibile a path must be provided.
 #' @param col_rename named character vector, with new names as values, and old names as names.c("new_name=old_name")
 #' @export
 #' @return data frame
 
-contin_results_csv <- function(col_rename=NULL) {
+contin_results_csv <- function(path=NULL, col_rename=NULL) {
 
-  files <- list.files(path=choose.dir(),pattern="*.csv", all.files=FALSE, recursive = FALSE, full.names = TRUE)
+  if(is.null(path) & exists('utils::choose.dir')){
+    # Windows
+    path=choose.dir()
+  }
+
+  if(is.null(path) & Sys.getenv("RSTUDIO") == "1"){
+    # Mac/Linux and running Rstudio
+    path=rstudioapi::selectDirectory()
+  } else {
+    stop("path must be provided.")
+  }
+
+  files <- list.files(path=path, pattern="*.csv", all.files=FALSE, recursive = FALSE, full.names = TRUE)
 
   df.all <- data.frame()
 
   for(file in files) {
 
-    df <- read.csv(file=file, header = TRUE)
+    df <- read.csv(file=file, header = TRUE, stringsAsFactors = FALSE)
 
     df.all <- rbind(df.all, df)
 
@@ -44,6 +58,10 @@ contin_results_csv <- function(col_rename=NULL) {
     df.all <- dplyr::rename(df.all, col_rename)
 
   }
+
+  # convert date and time columns to POSIXct, combine them
+  df.all$Activity.Start.Time <- as.POSIXct(paste(df.all$Activity.Start.Date, df.all$Activity.Start.Time), format="%Y/%m/%d %H:%M:%S", tz="UTC")
+  df.all$Activity.Start.Date <- df.all$Activity.Start.Time
 
   return(df.all)
 
