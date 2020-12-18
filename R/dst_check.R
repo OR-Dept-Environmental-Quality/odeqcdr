55e764aeee84f372d8bf6771852e03fed2181830
MICHIE Ryan <Ryan.Michie@deq.state.or.us>
2020-12-18 09:22:57 -0800

    improved methodology
    Now looks for errors when shifting from dst to standard time. Before only errors from the shift from standard time to dst were evaluated.
    Vastly improved methodology and code flow.

--- R/dst_check.R
+++ R/dst_check.R
@@ -1,14 +1,39 @@
 #' Check that date and time conform to changes between Daylight Time and Standard Time.
 #'
 #' This function will evaluate if an hour was added or subtracted correctly to account
-#' for the switch from daylight time to standard time and vice versa.
-#' For example in 2020, the switch from standard time to daylight time occurred
-#' on March 8, 2020 at 2:00 AM. In a time series where samples are collected
-#' every 30 minutes there should be no results recorded from 2:00 AM until 2:59 AM
-#' on March 8. If there are results recorded during this period it’s likely an adjustment
-#' for daylight savings was not made. This function attempts to find these occurrences and
-#' adds or subtracts an hour to all datetime values during the entire daylight savings
-#' or standard time period. A manual review of the results is recommend.
+#' for the switch from daylight time to standard time and vice versa. The check can only
+#' be used on timeseries occurring on or after the start of standard time on October 27, 1985 at 2:01 AM.
+#'
+#' The dst check fails and time is adjusted when either of the two conditions occur:
+#'
+#' 1. When there are results recorded between 2:00 AM and 2:59 AM when daylight savings begins.
+#'    As an example, in 2020 the switch from standard time to daylight time (+1 hour) occurred
+#'    on March 8, 2020 at 2:00 AM. In a time series where samples are collected
+#'    every 30 minutes there should be no results recorded from 2:00 AM until 2:59 AM.
+#'    If there are results recorded during this period it’s likely an adjustment
+#'    to daylight savings time was not made.
+#' 2. When there is only one result recorded for each datetime between 1:00 AM and 1:59 AM
+#'    after the transition back to standard time (-1 hour). As an example, in 2020 the switch from daylight savings
+#'    back to standard time occurred on November 1, 2020 at 2:00 AM resulting in a repeat of time
+#'    between 1:00 AM and 1:59 AM. If there are not duplicate times recorded it is likely
+#'    the adjustment to standard time was not made.
+#'
+#'When one of the conditions are detected in a timeseries, the time at the beginning
+#'of the deployment period is considered the correct time and the appropriate adjustment
+#'for daylight savings is made from that point forward. The dst check evaluates
+#'each deployment \[Monitoring.Location.ID, Characteristic.Name, Equipment.ID\]
+#'and any adjustments are made only to an individual deployment. A manual review of the results
+#'is recommend.
+#'
+#' Daylight Savings in Oregon since 1986
+#'
+#' 1986 - 2006:
+#'  Daylight savings time began by adding on hour (+1) on the first Sunday in April at 2:00 AM.
+#'  Daylight savings time ended by subtracting an hour (-1) on the last Sunday in October at 2:00 AM.
+#'
+#' 2007 - Current:
+#'  Daylight savings time begins by adding an hour (+1) on the second Sunday in March at 2:00 AM.
+#'  Daylight savings time ends by subtracting an hour (-1) on the first Sunday in November at 2:00 AM.
 #'
 #' @param df Data frame to be evaluated.
 #' @param mloc_col Column name in df holding the Monitoring Location ID. Default is "Monitoring.Location.ID".
@@ -20,21 +45,41 @@
 #' @export
 #' @return Vector of corrected datetime in POSISXct.
 
-dst_check <- function(df, mloc_col="Monitoring.Location.ID", char_col="Characteristic.Name", equip_col="Equipment.ID", date_col="Activity.Start.Date", time_col="Activity.Start.Time", tz_col="Activity.Start.End.Time.Zone") {
+dst_check <- function(df, mloc_col="Monitoring.Location.ID", char_col="Characteristic.Name",
+                      equip_col="Equipment.ID", date_col="Activity.Start.Date",
+                      time_col="Activity.Start.Time", tz_col="Activity.Start.End.Time.Zone") {
 
   #- Test---
-  # df_test <- data.frame(Monitoring.Location.ID= rep("ORDEQ-12345",5),
-  #                       Activity.Start.Date=lubridate::ymd(c(rep("2020/03/08", 5),
-  #                                                            rep("2020/11/01", 5)), tz = "UTC"),
-  #                       Activity.Start.Time=as.POSIXct(c("1:00:00", "1:30:00", "2:00:00", "2:30:00", "3:00:00",
-  #                                                        "1:00:00", "1:30:00", "2:00:00", "2:30:00", "3:00:00"),
-  #                                                      format = "%H:%M:%S", origin = "1970-01-01", tz ="UTC"),
-  #                       Activity.Start.End.Time.Zone=c("PST", "PST", "PDT", "PDT", "PDT",
-  #                                                      "PDT", "PDT", "PST", "PST", "PST"),
-  #                       Characteristic.Name=rep("Temperature, water", 10),
-  #                       Equipment.ID=rep("abc", 10),
-  #                       tz_name=rep('America/Los_Angeles', 10),
-  #                       tz_utc=rep('UTC', 10), stringsAsFactors = FALSE)
+
+  # # From data
+  # df_test <- df1.results
+  #
+  # # Needs to add hour in March PST -> PDT
+  # # Detected by findings NA
+  # dt <- as.POSIXct(c("2020/03/08 0:30:00", "2020/03/08 1:00:00", "2020/03/08 1:30:00", "2020/03/08 2:00:00", "2020/03/08 2:30:00", "2020/03/08 3:00:00",
+  #                    "2020/11/01 0:30:00", "2020/11/01 1:00:00", "2020/11/01 1:30:00", "2020/11/01 2:00:00", "2020/11/01 2:30:00", "2020/11/01 3:00:00"),
+  #                  format = "%Y/%m/%d %H:%M:%S", origin = "1970-01-01", tz ="UTC")
+  #
+  # # Needs to subtract hour in November PDT -> PST
+  # # Detected by lack of
+  # dt <- as.POSIXct(c("2020/03/08 0:30:00", "2020/03/08 1:00:00", "2020/03/08 1:30:00", "2020/03/08 3:00:00", "2020/03/08 3:30:00", "2020/03/08 4:00:00",
+  #                     "2020/11/01 0:30:00", "2020/11/01 1:00:00", "2020/11/01 1:30:00", "2020/11/01 2:00:00", "2020/11/01 2:30:00", "2020/11/01 3:00:00"),
+  #                   format = "%Y/%m/%d %H:%M:%S", origin = "1970-01-01", tz ="UTC")
+  #
+  # # correct
+  # dt <- as.POSIXct(c("2020/03/08 0:30:00", "2020/03/08 1:00:00", "2020/03/08 1:30:00", "2020/03/08 3:00:00", "2020/03/08 3:30:00", "2020/03/08 4:00:00",
+  #                    "2020/11/01 0:30:00", "2020/11/01 1:00:00", "2020/11/01 1:30:00", "2020/11/01 1:00:00", "2020/11/01 1:30:00", "2020/11/01 2:00:00"),
+  #                  format = "%Y/%m/%d %H:%M:%S", origin = "1970-01-01", tz ="UTC")
+  #
+  # df_test <- data.frame(Monitoring.Location.ID= rep("ORDEQ-12345",6),
+  #                       Activity.Start.Date=dt,
+  #                       Activity.Start.Time=dt,
+  #                       Activity.Start.End.Time.Zone=c("PST", "PST", "PST", "PDT", "PDT", "PDT",
+  #                                                      "PDT", "PDT", "PDT", "PST", "PST", "PST"),
+  #                       Characteristic.Name=rep("Temperature, water", 12),
+  #                       Equipment.ID=rep("abc", 12),
+  #                       tz_name=rep('America/Los_Angeles', 12),
+  #                       tz_utc=rep('UTC', 12), stringsAsFactors = FALSE)
   #
   # df_test$datetime <- odeqcdr::dt_combine(df=df_test,
   #                                         tz_col="tz_name")
@@ -67,9 +112,9 @@ dst_check <- function(df, mloc_col="Monitoring.Location.ID", char_col="Character
     stop(print(paste("char_col",char_col,"is not a column in df.")))
   }
 
-   if(!equip_col %in% names(df)) {
-     stop(print(paste("equip_col",equip_col,"is not a column in df.")))
-   }
+  if(!equip_col %in% names(df)) {
+    stop(print(paste("equip_col",equip_col,"is not a column in df.")))
+  }
 
   if(!date_col %in% names(df)) {
     stop(print(paste("date_col",date_col,"is not a column in df.")))
@@ -99,58 +144,101 @@ dst_check <- function(df, mloc_col="Monitoring.Location.ID", char_col="Character
                                          time_val = NULL,
                                          tz_col ="tz_utc")
 
+  if(min(df$datetime_utc, na.rm = TRUE) < as.POSIXct("1985/10/27 02:01:00", tz="UTC")) {
+    warning("df has datetimes before 2:01 AM on October 27, 1985. Only deployments that start on or after this date will be evaluated.")
+
+
+  }
+
   if(length(df$datetime_tz)==0) {
     stop("datetime has length of zero. No Change")
   }
 
   if(length(unique(lubridate::dst(df$datetime_tz))) == 1) {
 
-    print("No Time Change")
+    print("No time change. Timeseries in df does not switch between standard time and daylight savings time.")
 
     return(df$datetime_tz)
 
-    } else {
+  } else {
 
-      df2 <- select(df, -!!tz_col) %>%
-        cbind(lutz::tz_offset(df$datetime_tz)) %>%
-        dplyr::group_by_at(dplyr::vars(mloc_col, char_col, equip_col)) %>%
-        tidyr::fill(utc_offset_h, .direction="up") %>%
-        tidyr::fill(is_dst, .direction="up") %>%
-        dplyr::ungroup()
+    df2 <- dplyr::select(df, -!!tz_col) %>%
+      cbind(lutz::tz_offset(df$datetime_tz)) %>%
+      dplyr::group_by_at(dplyr::vars(mloc_col, char_col, equip_col)) %>%
+      tidyr::fill(utc_offset_h, .direction="up") %>%
+      tidyr::fill(is_dst, .direction="up") %>%
+      dplyr::mutate(dst_eval=dplyr::if_else(min(datetime_utc, na.rm = TRUE) >= as.POSIXct("1985/10/27 02:01:00", tz="UTC"), TRUE, FALSE)) %>%
+      dplyr::ungroup()
 
-      df3 <- df2 %>%
-        dplyr::filter(is.na(datetime_tz)) %>%
-        dplyr::select(!!mloc_col, !!char_col, !!equip_col, is_dst) %>%
-        dplyr::distinct() %>%
-        dplyr::mutate(hours_added=dplyr::if_else(is_dst,1,-1))
+    # Data frame of dates when DST time shift occurred during the period of data.
+    df.dst <- data.frame(date=seq(lubridate::ymd(format(min(df2$datetime_utc, na.rm = TRUE), "%Y-%m-%d")),
+                                  lubridate::ymd(format(max(df2$datetime_utc, na.rm = TRUE), "%Y-%m-%d")),
+                                  by = '1 day')) %>%
+      dplyr::mutate(year=lubridate::year(date),
+                    month=lubridate::month(date),
+                    wday=lubridate::wday(date, label=TRUE)) %>%
+      dplyr::group_by(year, wday, month) %>%
+      dplyr::mutate(dnum=dplyr::row_number()) %>%
+      dplyr::mutate(shift=dplyr::case_when(year > 1985 & year < 2007 & month==4 & wday=="Sun" & dnum==1 ~ 1,
+                                           year > 1985 & year < 2007 & month==10 & wday=="Sun" & dnum==max(dnum) ~ -1,
+                                           year > 2006 & month==3 & wday=="Sun" & dnum==2 ~ 1,
+                                           year > 2006 & month==11 & wday=="Sun" & dnum==1 ~ -1,
+                                           TRUE ~ 0)) %>%
+      dplyr::filter(shift %in% c(-1, 1)) %>%
+      dplyr::mutate(start=if_else(shift==1,date+hms("2:00:00"),date+hms("1:00:00")),
+                    stop=if_else(shift==1,date+hms("2:59:59"),date+hms("1:59:59"))) %>%
+      dplyr::mutate(date=format(date, "%Y-%m-%d"))
 
-      df4 <- df2 %>%
-        dplyr::left_join(df3) %>%
-        dplyr::mutate(hours_added=tidyr::replace_na(hours_added, 0),
-                      datetime_tz_fix=datetime_utc+lubridate::dhours(hours_added)) %>%
-        dplyr::group_by(tz_name) %>%
-        dplyr::mutate(datetime_tz_fix=lubridate::force_tz(time=datetime_tz_fix, tzone=unique(tz_name), roll = FALSE)) %>%
-        dplyr::ungroup()
+    df.start.tz <- df2 %>%
+      dplyr::group_by_at(dplyr::vars(mloc_col, char_col, equip_col)) %>%
+      dplyr::filter(min(datetime_utc)==datetime_utc) %>%
+      dplyr::select(deploy.start.tz=zone)
 
-      if(any(is.na(df2$datetime_tz))) {
-        print("DST break. Time change correction made for the following stations and periods:")
+    df.start.dst <- df2 %>%
+      dplyr::mutate(date=format(datetime_utc, "%Y-%m-%d")) %>%
+      dplyr::inner_join(df.dst) %>%
+      dplyr::filter(datetime_utc >= start & datetime_utc <= stop & shift == 1 & dst_eval) %>%
+      dplyr::group_by_at(dplyr::vars(mloc_col, char_col, equip_col, "datetime_utc")) %>%
+      dplyr::summarise(n=dplyr::n()) %>%
+      dplyr::filter(n > 0) %>%
+      dplyr::select(-n, -datetime_utc) %>%
+      dplyr::distinct() %>%
+      dplyr::mutate(dst_fail=TRUE)
 
-        df5 <- df4 %>%
-          dplyr::filter(is.na(datetime_tz)) %>%
-          dplyr::select(!!mloc_col, !!char_col, !!equip_col, datetime_utc, is_dst) %>%
-          dplyr::mutate(hours_added=dplyr::if_else(is_dst,1,-1)) %>%
-          dplyr::group_by_at(dplyr::vars(mloc_col, char_col, equip_col, hours_added, is_dst)) %>%
-          dplyr::summarize(start_datetime_fix=min(datetime_utc),
-                           end_datetime_fix=max(datetime_utc),
-                           n_fixed=n()) %>%
-          as.data.frame()
-        print(df5)
-      }
+    df.end.dst <- df2 %>%
+      dplyr::mutate(date=format(datetime_utc, "%Y-%m-%d")) %>%
+      dplyr::inner_join(df.dst) %>%
+      dplyr::filter(datetime_utc >= start &  datetime_utc <= stop & shift == -1 & dst_eval) %>%
+      dplyr::group_by_at(dplyr::vars(mloc_col, char_col, equip_col, "datetime_utc")) %>%
+      dplyr::summarise(n=dplyr::n()) %>%
+      dplyr::filter(n < 2) %>%
+      dplyr::select(-n, -datetime_utc) %>%
+      dplyr::distinct() %>%
+      dplyr::mutate(dst_fail=TRUE)
 
-      return(df4$datetime_tz_fix)
+    df.fail <- rbind(df.start.dst, df.end.dst) %>%
+      dplyr::distinct() %>%
+      dplyr::left_join(df.start.tz) %>%
+      as.data.frame()
+
+    # get offset at deployment convert to true UTC, and re-adjust to local timezone
+    df.fix <- df2 %>%
+      dplyr::group_by_at(dplyr::vars(mloc_col, char_col, equip_col)) %>%
+      dplyr::filter(min(datetime_utc)==datetime_utc) %>%
+      dplyr::select(!!mloc_col, !!char_col, !!equip_col, deploy_offset=utc_offset_h) %>%
+      dplyr::right_join(df2)%>%
+      dplyr::left_join(df.fail) %>%
+      dplyr::mutate(datetime_utc_fix=dplyr::if_else(dst_fail, datetime_utc+(-1*lubridate::dhours(deploy_offset)), datetime_utc)) %>%
+      dplyr::group_by(tz_name) %>%
+      dplyr::mutate(datetime_tz_fix=lubridate::with_tz(time=datetime_utc_fix, tzone=unique(tz_name)))
+
+    if(any(df.fix$dst_fail)) {
+      print("Time change correction made for the following deployments:")
+      print(df.fail)
+    }
+
+    return(df.fix$datetime_tz_fix)
 
   }
 
-
 }
-
