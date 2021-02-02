31ed0bc09b47240c07a2795754566fa0f323c4ac
Travis Pritchard <35708200+TravisPritchardODEQ@users.noreply.github.com>
2021-02-01 21:04:09 -0800

    Dql update (#15)
    * Add dql_update function
    
    * Update dql_update
    
    Update DQL updater function to account for results that already have comments. Build package componants.
    
    * Update volmon file
    
    UPdate volmon example file to account for changes in methodology.

---
+++ R/dql_update.R
@@ -0,0 +1,34 @@
+#' Manual update of rDQL field
+#'
+#' This function is to provide an ability to manually update rDQL field after manual review in shiny app for the volmon
+#' program. Input is intended to be the df4.results dataframe must contain the following fields: rDQL, row.results,
+#' Result.Comment
+#'
+#' @param df Dataframe to modify. Should be df4.results. No need to specify if part or pipe structure
+#' @param rows Rows to modify. Get these from reviewing the shinyapp
+#' @param DQL Updated final DQL value
+#' @param comment Comment to be used in Result.Comment field
+#' @export
+#' @return Dataframe with updated DQL and comment
+#'
+#'
+
+update_DQL <- function(df, rows, DQL, comment = NA_character_){
+
+if(!DQL %in% c("A", 'B', 'C', 'D', 'E', 'F')){
+  stop("Invalid DQL. Please update using valid DQL")
+}
+
+
+  df2 <- df %>%
+    dplyr::mutate(rDQL = dplyr::case_when(row.results %in% rows ~ DQL,
+                                          TRUE ~ rDQL),
+                  Result.Comment = dplyr::case_when(row.results %in% rows & is.na(Result.Comment) ~ comment,
+                                                    row.results %in% rows & !is.na(Result.Comment) & !is.na(comment) ~ paste(Result.Comment, ";", comment),
+                                                    TRUE ~ Result.Comment
+                                                    )) %>%
+
+
+  return(df2)
+
+}
